9ac9ec214ced31ba7dda2fb28a5ea7cd14012d2a
ArtemSamosadov <pizzaeueu@gmail.com>
2017-04-08 10:58:14 +0300

    add cors

--- src/main/java/com/jjsbot/JjsbotApplication.java
+++ src/main/java/com/jjsbot/JjsbotApplication.java
@@ -2,11 +2,29 @@ package com.jjsbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class JjsbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JjsbotApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+						.allowedHeaders("header1", "header2") //What is this for?
+						.allowCredentials(true);
+			}
+		};
 	}
 }
