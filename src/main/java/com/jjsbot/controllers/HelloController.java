cf33677096a7c829b1ec54f4aa09997110d8cc61
ArtemSamosadov <pizzaeueu@gmail.com>
2017-04-08 12:33:44 +0300

    add message handler

--- src/main/java/com/jjsbot/controllers/HelloController.java
+++ src/main/java/com/jjsbot/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.jjsbot.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String hello(){
+    public String hello() {
         return "7de442fd";
     }
 
+    @RequestMapping(value = "/ab", method = RequestMethod.POST)
+    public String activateBot() {
+        return "587d5fe4";
+    }
 }
