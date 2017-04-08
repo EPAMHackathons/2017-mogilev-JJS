db6fcdd7e8a5bd3ca3fd8aad6fe7ef7b4f178ec7
ArtemSamosadov <pizzaeueu@gmail.com>
2017-04-08 09:32:32 +0300

    init

---
+++ src/main/java/com/jjsbot/JjsbotApplication.java
@@ -0,0 +1,12 @@
+package com.jjsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjsbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjsbotApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jjsbot/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.jjsbot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Admin on 02.04.2017.
+ */
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+}
