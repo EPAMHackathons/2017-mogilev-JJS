97c59ab9abffb13c4d757eb7a4e150e1bf6ed606
ArtemSamosadov <pizzaeueu@gmail.com>
2017-05-04 09:46:34 +0300

    fdas

---
+++ src/main/java/com/jjsbot/controllers/TokenController.java
@@ -0,0 +1,64 @@
+package com.jjsbot.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jjsbot.models.Person;
+import com.jjsbot.models.TokenResponse;
+import com.jjsbot.services.PersonService;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Admin on 02.05.2017.
+ */
+@RestController
+@RequestMapping("/api/v1/token")
+public class TokenController {
+
+    PersonService personService = new PersonService();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView method() {
+        return new ModelAndView("redirect:" + "https://oauth.vk.com/authorize?client_id=5972860&redirect_uri=http://localhost:8080/api/v1/token/get");
+
+    }
+
+    @RequestMapping(value="/get", method = RequestMethod.GET)
+    public ResponseEntity<Person> getToken(@RequestParam(value = "code") String code, HttpServletRequest request, HttpServletResponse response) throws IOException, ClientException, ApiException, InterruptedException {
+        //return new ModelAndView("redirect:" + "https://oauth.vk.com/authorize?client_id=5972860&redirect_uri=http://localhost:8080/api/v1/token/get");
+        String url = request.getRequestURL().toString() + "?" + request.getQueryString();
+        if (url.contains("access_token")){
+            System.out.println("YEAAAHH");
+            System.out.println(response);
+            return null;
+        } else {
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println(code);
+            String urlToGetToken = "https://oauth.vk.com/access_token?client_id=5972860&client_secret=R3ya8Wu2XQkw8rlPN5Hi&redirect_uri=http://localhost:8080/api/v1/token/get&code="+code;
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(urlToGetToken, String.class);
+            TokenResponse token = mapper.readValue(result, TokenResponse.class);
+
+            Person person = personService.getPersonById(token.getUser_id(), token.getAccess_token(), 2);
+            //String json = personService.arrangeToOlegJson(person);
+
+            personService.counter = 0;
+
+            return ResponseEntity.ok(person);
+        }
+
+    }
+}
---
+++ src/main/java/com/jjsbot/models/TokenResponse.java
@@ -0,0 +1,35 @@
+package com.jjsbot.models;
+
+/**
+ * Created by Admin on 02.05.2017.
+ */
+public class TokenResponse {
+
+    private String access_token;
+    private Integer expires_in;
+    private Integer user_id;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public Integer getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(Integer expires_in) {
+        this.expires_in = expires_in;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+}
